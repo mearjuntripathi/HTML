f68ac4d8abf1d5c1fc20a1c03e1b0a2939426471
mearjuntripathi <rajr60666@gmail.com>
2023-05-03 21:04:38 +0530

    new update

--- voiceassistant/script.js
+++ voiceassistant/script.js
@@ -9,7 +9,11 @@ recognition.interimResults = true;
 
 let speakData = new SpeechSynthesisUtterance();
 
+let gen = true;
+
 function speaks(message){
+    if(gen == false)
+        return;
     let textToSpeak = message;
     speakData.volume = 1; // From 0 to 1
     speakData.rate = 1; // From 0.1 to 10
@@ -20,12 +24,13 @@ function speaks(message){
 
 function writeAnswer(txt){
     var i = 0;
-
     var speed = 50;
-
     answer.innerHTML = "";
-
     function typeWriter() {
+        if(gen == false){
+            gen = true;
+            return;
+        }
         if (i < txt.length) {
             answer.innerHTML += txt.charAt(i);
             i++;
@@ -35,51 +40,34 @@ function writeAnswer(txt){
     typeWriter();
 }
 
-
-
 speaks("Hi I am DORA, a virtual voice Assistent, Tell me how can i help you");
 
+document.getElementById('picture').addEventListener('click', ()=>{ recognition.start() })
 
-// const API_KEY = "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";
-// const API_URL = "https://api.openai.com/v1/engines/text-davinci-002/jobs";
 
-// async function generateResponse(prompt) {
-//   return new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open("POST", API_URL, true);
-//     xhr.setRequestHeader("Authorization", `Bearer ${API_KEY}`);
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     xhr.onreadystatechange = function () {
-//       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-//         const response = JSON.parse(xhr.responseText);
-//         resolve(response.choices[0].text);
-//       }
-//     };
-//     xhr.send(JSON.stringify({
-//       prompt: prompt,
-//       max_tokens: 100,
-//       n: 1,
-//       stop: null,
-//       temperature: 0.5,
-//     }));
-//   });
-// }
+document.getElementById('genrated').addEventListener('click', ()=>{
+    gen = false;
+    document.getElementById('genrated').style.visibility = 'hidden';
+    content.innerHTML = '"Ask your question"';
+    recognition.start();
+})
 
-// async function main(prompt) {
-//   const response = await generateResponse(prompt);
-//   console.log(response);
-//   return response;
-// }
+function generateResponse(txt){
+    document.getElementById('genrated').style.visibility = 'visible';
+    speaks(txt);
+    writeAnswer(txt);
+}
 
 recognition.addEventListener('result',(e)=>{
     const text = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join('') 
-    // console.log(text);
     content.innerHTML = text;
+
     if(e.results[0].isFinal){
+
         if(text.toLowerCase().includes("hi dora") || text.toLowerCase().includes("hello dora")){
-            speaks("Hi, How I can Help you");
-            writeAnswer("Hi, How I can Help you");
+            generateResponse("Hi, How I can Help you");
         }
+
         else if(text.toLowerCase().includes("time")){
             var hours = new Date().getHours();
             var minutes = new Date().getMinutes();
@@ -89,22 +77,17 @@ recognition.addEventListener('result',(e)=>{
             minutes = minutes < 10 ? '0'+minutes : minutes;
             var strTime = hours + ':' + minutes + ' ' + ampm;
             strTime = "Is it "+strTime;
-            speaks(strTime);
-            writeAnswer(strTime);
+            generateResponse(strTime);
         }
+
         else if(text.toLowerCase().includes("date")){
             let today = new Date().toISOString().slice(0, 10);
             today = "Today is "+today;
-            speaks(today);
-            writeAnswer(today);
+            generateResponse(today);
         }
-        // else{
-        //     speaks("Sorry, I am not fully ready, I am in a development mode at this time");
-        //     writeAnswer("Sorry, I am not fully ready, I am in a development mode at this time");
-            
-        // }
+
         else if(text.toLowerCase().includes("search")){
-            speaks("Okey i search "+text.replace("search", "")+"on Google");
+            speaks("Okey i search here i got some solution for you"+text.replace("search", "")+"on Google");
             window.open(`http://google.com/search?q=${text.replace("search", "")}`, "_blank");
         }
         else if(text.toLowerCase().includes("open")){
@@ -137,23 +120,21 @@ recognition.addEventListener('result',(e)=>{
                 response = response.query.pages;
                 var pageid = Object.keys(response)[0];
                 var result = response[pageid].extract;
-                speaks(result);
-                writeAnswer(result);
+                generateResponse(result);
             })
         }
         else{
             // const response = main(text);
             var response = "I am in development mode so sorry i can't tell you a solution of this question";
-            speaks(response);
-            writeAnswer(response);
+            generateResponse(response);
             content.innerHTML = '"Ask your question"';
         }
-        content.innerHTML = '"Ask your question"';
-    }
+        recognition.interimResults = false;
+        }
 });
 
-recognition.addEventListener('end', ()=>{
-    recognition.start();
-})
+// recognition.addEventListener('end', ()=>{
+//     recognition.start();
+// })
 
 recognition.start();
