ad526d6d695b9c36984b85f95bd8878b1c23e37b
mearjuntripathi <mearjuntripathi@gmail.com>
2023-10-14 21:00:40 +0530

    new Portfolio updated

---
+++ index.js
@@ -0,0 +1,162 @@
+function writeAnswer() {
+    var _CONTENT = [
+        "a passionate software engineer!",
+        "As a fresh graduate with a strong foundation",
+        "Having a month of Internship Experience",
+        "in Object-Oriented Programming (OOP),",
+        "Data Structures and Algorithms (DSA),",
+        "and Database management,",
+        "Backend Development with PHP, Node/Express, Golang",
+        "I'm ready to embark on a journey",
+        "in the world of technology.",
+        "My curiosity drives me to solve complex problems",
+        "and create elegant solutions.",
+        "Explore my portfolio to see how I've applied",
+        "my skills and knowledge to real-world projects.",
+        "Let's connect and discuss how I can contribute",
+        "to your next software development endeavor.",
+        ];
+    // Current sentence being processed
+    var _PART = 0;
+    // Character number of the current sentence being processed 
+    var _PART_INDEX = 0;
+    // Holds the handle returned from setInterval
+    var _INTERVAL_VAL;
+    // Element that holds the text
+    var _ELEMENT = document.querySelector("#message p");
+    // Implements typing effect
+    function Type() {
+        var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX++;
+        // If full sentence has been displayed then start to delete the sentence after some time
+        if (text === _CONTENT[_PART]) {
+            clearInterval(_INTERVAL_VAL);
+            setTimeout(function () {
+                _INTERVAL_VAL = setInterval(Delete, 20);
+            }, 1000);
+        }
+    }
+    // Implements deleting effect
+    function Delete() {
+        var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX--;
+        // If sentence has been deleted then start to display the next sentence
+        if (text === '') {
+            clearInterval(_INTERVAL_VAL);
+            // If last sentence then display the first one, else move to the next
+            if (_PART == (_CONTENT.length - 1))
+                _PART = 0;
+            else
+                _PART++;
+            _PART_INDEX = 0;
+
+            // Start to display the next sentence after some time
+            setTimeout(function () {
+                _INTERVAL_VAL = setInterval(Type, 50);
+            }, 200);
+        }
+    }
+    // Start the typing effect on load
+    _INTERVAL_VAL = setInterval(Type, 100);
+}
+
+window.onload = function(){
+    document.getElementById('nav').classList.add('dark');
+    writeAnswer();
+}
+
+document.querySelector('#mode').addEventListener('click', ()=>{
+    if(document.querySelector('#mode').innerHTML ==  '<i class="fa fa-moon-o fa-2x" style="color: white"></i>'){
+        document.getElementsByTagName('body')[0].classList = 'dark';
+        document.getElementById('nav').classList.add('dark');
+        document.getElementById('nav').classList.remove('light');
+        document.querySelector('#mode').innerHTML = `<i class="fa fa-sun-o fa-2x" style="color: yellow;"></i>`
+    }else{
+        document.getElementsByTagName('body')[0].classList = 'light';
+        document.getElementById('nav').classList.add('light');
+        document.getElementById('nav').classList.remove('dark');
+        document.querySelector('#mode').innerHTML = `<i class="fa fa-moon-o fa-2x" style="color: white"></i>`   
+    }
+})
+
+const bars = document.querySelector('.bars');
+bars.addEventListener('click',function(){
+    if(bars.innerHTML == '<img src="image/profile.JPG">'){
+        document.getElementById('nav').classList.add('shownav');
+        bars.innerHTML = '<i class="fa fa-times"></i>';
+    }
+    else{
+        document.getElementById('nav').classList.remove('shownav');
+        bars.innerHTML = '<img src="image/profile.JPG">'
+    }
+})
+
+document.querySelector('#connect').addEventListener('click',()=>{
+    if(document.querySelector('.social').className.includes('hide')){
+        document.querySelector('.social').classList.remove('hide');
+    }
+    else{
+        document.querySelector('.social').classList.add('hide');
+    }
+})
+
+// nav scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        // Remove "active" class from all links
+        document.querySelectorAll('a[href^="#"]').forEach(link => {
+            link.classList.remove('active');
+        });
+
+        // Add "active" class to the clicked link
+        this.classList.add('active');
+        if(document.getElementById('nav').className.includes('shownav')){
+            bars.click();
+        }
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.querySelector('.'+targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Get all anchor links and their corresponding divs
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+const divs = Array.from(anchorLinks, link => {
+  const targetId = link.getAttribute('href').substring(1);
+  return {
+    link,
+    div: document.querySelector('.' + targetId),
+  };
+});
+
+// Function to update the active link based on scroll position
+function updateActiveLink() {
+  const scrollPosition = window.scrollY;
+  for (const { link, div } of divs) {
+    const divTop = div.offsetTop;
+    const divBottom = divTop + div.clientHeight;
+
+    if (scrollPosition >= divTop && scrollPosition < divBottom) {
+      // Remove "active" class from all links
+      anchorLinks.forEach(anchor => anchor.classList.remove('active'));
+
+      // Add "active" class to the link corresponding to the current div
+      link.classList.add('active');
+    }
+  }
+}
+
+// Add scroll event listener to update active link on scroll
+window.addEventListener('scroll', updateActiveLink);
+
+// Initial update
+updateActiveLink();
---
+++ script.js
@@ -0,0 +1,179 @@
+document.querySelector('#mode').addEventListener('click', () => {
+    if (document.querySelector('#mode').innerHTML == '<i class="fa fa-moon-o fa-2x" style="color: white"></i>') {
+        document.getElementsByTagName('body')[0].classList = 'dark';
+        document.querySelector('#mode').innerHTML = `<i class="fa fa-sun-o fa-2x" style="color: yellow;"></i>`;
+    } else {
+        document.getElementsByTagName('body')[0].classList = 'light';
+        document.querySelector('#mode').innerHTML = `<i class="fa fa-moon-o fa-2x" style="color: white"></i>`;
+    }
+})
+
+let recent = [
+    { "topic": "DSA NOTES", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm?tab=readme-ov-file#data-structure--algorithm" },
+    { "topic": "RestAPI", "tech": "nodejs • js", "live": "", "docs": "https://github.com/mearjuntripathi/nodejs/tree/main/crud-RESTAPI" },
+    { "topic": "JAVA Digital Clock", "tech": "JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Java/blob/main/DigitalClock.java" },
+    { "topic": "Chat ROOM", "tech": "JAVA • socket.io", "live": "", "docs": "https://github.com/mearjuntripathi/Java/tree/main/Chat-Room" },
+    { "topic": "RestAPI/ginAPI", "tech": "GOlang • ginAPI", "live": "", "docs": "https://github.com/mearjuntripathi/GOLang" },
+    { "topic": "Blog Website", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/blog-writer/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/blog-writer" },
+    { "topic": "Chat BOT", "tech": "HTML CSS JS", "live": "https://mearjuntripathi.github.io/chatbot/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/chatbot" }
+];
+
+let dev = [
+    { "topic": "Chat Application", "tech": "HTML • CSS • JS • PHP • JQuery • mySQL", "live": "https://greatchatson.000webhostapp.com/", "docs": "https://github.com/mearjuntripathi/PHP/tree/main/chat_on" },
+    { "topic": "RestAPI", "tech": "nodejs • js", "live": "", "docs": "https://github.com/mearjuntripathi/nodejs/tree/main/crud-RESTAPI" },
+    { "topic": "Chat ROOM", "tech": "JAVA • socket.io", "live": "", "docs": "https://github.com/mearjuntripathi/Java/tree/main/Chat-Room" },
+    { "topic": "RestAPI/ginAPI", "tech": "GOlang • ginAPI", "live": "", "docs": "https://github.com/mearjuntripathi/GOLang" },
+    { "topic": "Blog Website", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/blog-writer/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/blog-writer" },
+    { "topic": "Chat BOT", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/chatbot/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/chatbot" },
+    { "topic": "Voice Assistent", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/dora/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/dora" },
+    { "topic": "Strange video call", "tech": "HTML • CSS • JS • ScaleDrone", "live": "https://mearjuntripathi.github.io/stranger/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/stranger" },
+    { "topic": "Basic Calculator", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/calculator/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/calculator" },
+    { "topic": "Image to PDF Converter", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/imagetopdf/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/imagetopdf" },
+    { "topic": "To-DO List", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/TO-DO%20list/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/TO-DO%20list" },
+    { "topic": "Tic Tac Toe", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/tiktactoe/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/tiktactoe" },
+    { "topic": "Code Editor", "tech": "HTML • CSS • JS", "live": "https://mearjuntripathi.github.io/codeeditor/", "docs": "https://github.com/mearjuntripathi/mearjuntripathi.github.io/tree/main/calculator" }
+];
+
+let dsa = [
+    { "topic": "Introduction DSA", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm" },
+    { "topic": "Stack", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Stack" },
+    { "topic": "Queue", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Queue" },
+    { "topic": "Sorting", "tech": "C++", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Sorting" },
+    { "topic": "Searching", "tech": "JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Searching" },
+    { "topic": "Single LinkList", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Linked%20List/single%20linkedlist" },
+    { "topic": "Double LinkList", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Linked%20List/Doubly%20linkedlist" },
+    { "topic": "Tree", "tech": "C++", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Tree" },
+    { "topic": "Binary Tree", "tech": "C++", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Tree/Binary%20Tree" },
+];
+
+let java = [
+    { "topic": "Chat ROOM", "tech": "JAVA • socket.io", "live": "", "docs": "https://github.com/mearjuntripathi/Java/tree/main/Chat-Room" },
+    { "topic": "DSA NOTES", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm?tab=readme-ov-file#data-structure--algorithm" },
+    { "topic": "JAVA Digital Clock", "tech": "JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Java/blob/main/DigitalClock.java" },
+    { "topic": "Stack", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Stack" },
+    { "topic": "Queue", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Queue" },
+    { "topic": "Searching", "tech": "JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Searching" },
+    { "topic": "Single LinkList", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Linked%20List/single%20linkedlist" },
+    { "topic": "Double LinkList", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Linked%20List/Doubly%20linkedlist" },
+    
+];
+
+let cpp = [
+    { "topic": "Introduction DSA", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm" },
+    { "topic": "Stack", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Stack" },
+    { "topic": "Queue", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Queue" },
+    { "topic": "Sorting", "tech": "C++", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Sorting" },
+    { "topic": "Single LinkList", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Linked%20List/single%20linkedlist" },
+    { "topic": "Double LinkList", "tech": "C++ • C • JAVA", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Linked%20List/Doubly%20linkedlist" },
+    { "topic": "Tree", "tech": "C++", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Tree" },
+    { "topic": "Binary Tree", "tech": "C++", "live": "", "docs": "https://github.com/mearjuntripathi/Data_Structure_Algorithm/tree/main/Tree/Binary%20Tree" },
+    { "topic": "Graphics Quiz Game", "tech": "C", "live": "", "docs": "https://github.com/mearjuntripathi/Personal-content/tree/master/QUIZ%20GAME" },
+    { "topic": "Dynamic Memory Allocation", "tech": "C", "live": "", "docs": "https://github.com/mearjuntripathi/Personal-content/blob/master/C/DynamicMamory.c" }
+];
+
+let nodejs = [
+    { "topic": "RestAPI", "tech": "nodejs • js", "live": "", "docs": "https://github.com/mearjuntripathi/nodejs/tree/main/crud-RESTAPI" }
+];
+
+let go = [
+    { "topic": "RestAPI/ginAPI", "tech": "GOlang • ginAPI", "live": "", "docs": "https://github.com/mearjuntripathi/GOLang" },
+    { "topic": "gRPC chat microservice", "tech": "GOlang • gRPC", "live": "", "docs": "https://github.com/mearjuntripathi/grpc-chat-microservice" }
+];
+
+let database = [
+    { "topic": "Chat Application", "tech": "HTML • CSS • JS • PHP • JQuery • mySQL", "live": "https://greatchatson.000webhostapp.com/", "docs": "https://github.com/mearjuntripathi/PHP/tree/main/chat_on" },
+
+];
+
+const projects = document.getElementById('projects');
+
+function checklive(data){
+    if(data == "")
+        return "";
+    else return `<a target="_blank" href="${data}" class="live"><i
+    class="fa fa-eye"> Live website</i></a>`
+}
+
+function addProject(data){
+    let project = document.createElement('div');
+    project.classList.add('project');
+    let info = `
+    <h1 class="title">${data.topic}</h1>
+                <hr>
+                <p class="used-tech">
+                    Used Technology: 
+                    <span>
+                        ${data.tech}
+                    </span>
+                </p>
+                <div class="view">
+                    `+
+                    checklive(data.live)
+                    +`
+                    <a target="_blank"
+                        href="${data.docs}"
+                        class="documentation"><i class="fa fa-file"> Documentation</i></a>
+                </div>
+            </div>
+    `;
+    project.innerHTML = info;
+    projects.append(project);
+}
+
+const operation = document.querySelectorAll('.topic button');
+
+for (let i = 0; i < operation.length; i++) {
+    operation[i].addEventListener('click', function () {
+        document.querySelector('.active').classList.remove('active');
+        projects.innerHTML = "";
+        switch(operation[i].name){
+            case "recent":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < recent.length ; i++)
+                    addProject(recent[i]);
+            break;
+
+            case "dev":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < dev.length ; i++)
+                    addProject(dev[i]);
+            break;
+
+            case "dsa":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < dsa.length ; i++)
+                    addProject(dsa[i]);
+            break;
+
+            case "java":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < java.length ; i++)
+                    addProject(java[i]);
+            break;
+
+            case "cpp":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < cpp.length ; i++)
+                    addProject(cpp[i]);
+            break;
+
+            case "nodejs":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < nodejs.length ; i++)
+                    addProject(nodejs[i]);
+            break;
+
+            case "go":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < go.length ; i++)
+                    addProject(go[i]);
+            break;
+
+            case "database":
+                operation[i].classList.add('active');
+                for(let i = 0 ; i < database.length ; i++)
+                    addProject(database[i]);
+            break;
+        }
+    })
+}
