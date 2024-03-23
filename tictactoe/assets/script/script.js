a793536b0282cd9e52721226fb50afd3093b91e4
mearjuntripathi <mearjuntripathi@gmail.com>
2024-03-23 20:22:52 +0530

    new changes

--- tictactoe/assets/script/script.js
+++ tictactoe/assets/script/script.js
@@ -14,10 +14,10 @@ function startGame(btn) {
     let btn2 = document.createElement('button');
     let btn3 = document.createElement('button');
     btn1.innerHTML = '<a href="twoPlayer.html">Two Player</a>';
-    btn2.innerText = 'https://tic-tac-toe-k2oh.onrender.com/multiplayer';
+    btn2.innerText = '<a href="https://tic-tac-toe-k2oh.onrender.com/multiplayer">Online Multiplayer</a>';
     btn3.innerHTML = '<a href="computer.html">Play Against Computer</a>';
     div.appendChild(btn1);
-    div.appendChild(btn2);
+    // div.appendChild(btn2);
     div.appendChild(btn3);
     container.removeChild(btn);
     container.appendChild(div);
